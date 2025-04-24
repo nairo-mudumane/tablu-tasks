fc536812f3c3939d1f64a4df4449248d1df53619
Nairo Mudumane <nazirmudumane@gmail.com>
2025-04-24 22:46:03 +0200

    fix: lint errors

--- server-node/src/index.ts
+++ server-node/src/index.ts
@@ -1,18 +1,19 @@
-import "dotenv/config";
+import 'dotenv/config';
 
-import express from "express";
-import cors from "cors";
+import express from 'express';
+import cors from 'cors';
 
-import { ENV } from "./lib/dotenv";
+import { ENV } from './lib/dotenv';
 
 const app = express();
 const PORT = ENV.PORT;
 const allowedOrigins =
-  ENV.NODE_ENV === "prod" ? ENV.ALLOWED_ORIGINS.split(",") : ["*"];
+  ENV.NODE_ENV === 'prod' ? ENV.ALLOWED_ORIGINS.split(',') : ['*'];
 
 app.use(cors({ origin: allowedOrigins }));
 app.use(express.json());
 
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`server running on ${ENV.PORT}`);
 });
--- server-node/src/lib/dotenv.ts
+++ server-node/src/lib/dotenv.ts
@@ -1,10 +1,10 @@
-import "dotenv/config";
+import 'dotenv/config';
 
-import { z } from "zod";
+import { z } from 'zod';
 
 const schema = z.object({
   PORT: z.union([z.string(), z.number()]),
-  NODE_ENV: z.enum(["dev", "stg", "prod"]).default("dev"),
+  NODE_ENV: z.enum(['dev', 'stg', 'prod']).default('dev'),
   ALLOWED_ORIGINS: z.string(),
 });
 
