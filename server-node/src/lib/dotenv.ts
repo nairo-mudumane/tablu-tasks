9c0912d1cc906700b40d294f72ab63abd7ee7433
Nairo Mudumane <nazirmudumane@gmail.com>
2025-04-25 01:05:28 +0200

    bug: can't run migrations

--- server-node/src/lib/dotenv.ts
+++ server-node/src/lib/dotenv.ts
@@ -6,6 +6,13 @@ const schema = z.object({
   PORT: z.union([z.string(), z.number()]),
   NODE_ENV: z.enum(['dev', 'stg', 'prod']).default('dev'),
   ALLOWED_ORIGINS: z.string(),
+  DB_HOST: z.string(),
+  DB_PORT: z.union([z.string(), z.number()]),
+  DB_USER: z.string(),
+  DB_PASSWORD: z.string(),
+  DB_ROOT_PASSWORD: z.string(),
+  DB_NAME: z.string(),
+  DATABASE_URL: z.string().url(),
 });
 
 export const ENV = schema.parse(process.env);
